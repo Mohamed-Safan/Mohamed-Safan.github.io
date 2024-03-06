4498bcc079c689d55ffd5c4680bdf91c8420d9f1
Mohamed Safan <66354960+Mohamed-Safan@users.noreply.github.com>
2024-03-06 19:59:31 +0530

    update summary

--- contact.php
+++ contact.php
@@ -17,7 +17,7 @@
         }
 
         // Update this to your desired email address.
-        $recipient = "contact@yourdomain.com";
+        $recipient = "zaaslabs@gmail.com";
 		$subject = "Message from $name";
 
         // Email content.
